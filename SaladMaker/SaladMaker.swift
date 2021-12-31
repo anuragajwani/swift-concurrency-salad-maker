dfd823800a42aa01c9394344b1be74e58f15613c
Anurag Ajwani <anurag.ajwani@gmail.com>
2021-12-31 00:17:20 +0000

    Now using async let's to prepare salad ingredients in parallel

--- SaladMaker/SaladMaker.swift
+++ SaladMaker/SaladMaker.swift
@@ -10,20 +10,23 @@ import Foundation
 class SaladMaker {
     func make(onIngrdientPrepped: @escaping (Ingredient) -> ()) {
         Task(priority: .userInitiated) {
-            for ingredient in Ingredient.allCases {
-                await prepareIngredient(ingredient)
-                onIngrdientPrepped(ingredient)
-            }
+            async let lettuce = prepareIngredient(.lettuce)
+            async let tomatoes = prepareIngredient(.tomatoes)
+            async let redOnion = prepareIngredient(.redOnion)
+            async let sweetcorn = prepareIngredient(.sweetcorn)
+            async let tuna = prepareIngredient(.tuna)
+            let ingredients = await [lettuce, tomatoes, redOnion, sweetcorn, tuna]
+            ingredients.forEach(onIngrdientPrepped)
         }
     }
 
-    private func prepareIngredient(_ ingredient: Ingredient) async {
+    private func prepareIngredient(_ ingredient: Ingredient) async -> Ingredient {
         return await withCheckedContinuation({ continuation in
             // using DispatchQueue here instead of Task due to unexpected behaviour
             DispatchQueue.global(qos: .userInitiated).async {
                 let randomTime = UInt32.random(in: 1...5)
                 sleep(randomTime)
-                continuation.resume()
+                continuation.resume(returning: ingredient)
             }
         })
     }
